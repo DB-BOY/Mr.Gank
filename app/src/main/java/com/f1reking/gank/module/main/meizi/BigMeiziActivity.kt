7d51f054238f472d97a71f557075642dd0d07c37
F1ReKing <284061805@qq.com>
2018-01-24 17:28:50 +0800

    [优化] 添加版权

--- app/src/main/java/com/f1reking/gank/module/main/meizi/BigMeiziActivity.kt
+++ app/src/main/java/com/f1reking/gank/module/main/meizi/BigMeiziActivity.kt
@@ -1,3 +1,19 @@
+/*
+ *  Copyright (c) 2018 F1ReKing
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.f1reking.gank.module.main.meizi
 
 import android.Manifest
@@ -23,7 +39,7 @@ import kotlinx.android.synthetic.main.activity_big_meizi.iv_pic
 import kotlinx.android.synthetic.main.toolbar.toolbar
 
 /**
- * @author: huangyh
+ * @author: F1ReKing
  * @date: 2018/1/9 21:04
  * @desc: 大妹子图片
  */
--- app/src/main/java/com/f1reking/gank/room/AppDatabaseHelper.kt
+++ app/src/main/java/com/f1reking/gank/room/AppDatabaseHelper.kt
@@ -1,3 +1,19 @@
+/*
+ *  Copyright (c) 2018 F1ReKing
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.f1reking.gank.room
 
 import android.arch.persistence.room.Room
@@ -5,7 +21,7 @@ import android.content.Context
 import com.f1reking.gank.entity.CollectionEntity
 
 /**
- * @author: huangyh
+ * @author: F1ReKing
  * @date: 2018/1/23 10:45
  * @desc:
  */
